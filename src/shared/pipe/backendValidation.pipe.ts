625b904cd321c34c1e9a2f4de947472cf282025c
Datsede04 <Datsede04@gmail.com>
2022-03-21 23:12:56 +0300

    checking type is added on backend validation

--- src/shared/pipe/backendValidation.pipe.ts
+++ src/shared/pipe/backendValidation.pipe.ts
@@ -11,6 +11,10 @@ import { validate } from 'class-validator';
 export class BackendValidationPipe implements PipeTransform {
   async transform(value: any, metadata: ArgumentMetadata) {
     const object = plainToClass(metadata.metatype, value);
+
+    if (typeof object !== 'object') {
+      return value;
+    }
     const error = await validate(object);
 
     if (error.length === 0) {
